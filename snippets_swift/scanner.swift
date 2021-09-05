89513c9f30131350603abc17da451df1db99e8f7
semisagi0 <semisagi0@gmail.com>
2021-09-05 20:30:06 +0900

    ScannerにnextStrings()を実装。

--- snippets_swift/scanner.swift
+++ snippets_swift/scanner.swift
@@ -1,30 +1,37 @@
 // snippet_id: 8d82878e-4927-4f5a-a7f3-51c9668553a2
+// https://github.com/semisagi0/kyopro-snippet
 struct Scanner {
     private var elements = [String]()
     private var index = 0
 
     mutating func peek() -> String {
         while elements.count == index {
-            elements = readLine()!.split(separator: " ").map(String.init)
+            elements = readLine()!.split(separator: " ").map(\.description)
             index = 0
         }
         return elements[index]
     }
 
-    mutating func next() -> String {
-        defer { index += 1 }
+    mutating func nextString() -> String {
+        defer {
+            index += 1
+        }
         return peek()
     }
 
-    mutating func nextInt() -> Int {
-        return Int(next())!
+    mutating func nextStrings(count n: Int) -> [String] {
+        return (0 ..< n).map { _ in nextString() }
     }
 
-    mutating func nextInts(_ n: Int) -> [Int] {
+    mutating func nextInt() -> Int {
+        return Int(nextString())!
+    }
+
+    mutating func nextInts(count n: Int) -> [Int] {
         return (0 ..< n).map { _ in nextInt() }
     }
 
     mutating func nextDouble() -> Double {
-        return Double(next())!
+        return Double(nextString())!
     }
 }
