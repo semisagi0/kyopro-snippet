9a240e20feafceacd8003cd37b2a5eb40b41b7f6
semisagi0 <semisagi0@gmail.com>
2022-02-26 13:28:54 +0900

    Mainを追加

---
+++ snippets_swift/main.swift
@@ -0,0 +1,53 @@
+func main() {
+    var scanner = Scanner()
+}
+
+main()
+
+// snippet_id: 8d82878e-4927-4f5a-a7f3-51c9668553a2
+// https://github.com/semisagi0/kyopro-snippet
+struct Scanner {
+    private var elements = [String]()
+    private var index = 0
+    
+    mutating func peek() -> String {
+        while elements.count == index {
+            elements = readLine()!.split(separator: " ").map(\.description)
+            index = 0
+        }
+        return elements[index]
+    }
+    
+    mutating func nextString() -> String {
+        defer {
+            index += 1
+        }
+        return peek()
+    }
+    
+    mutating func nextStrings(count n: Int) -> [String] {
+        return (0 ..< n).map { _ in nextString() }
+    }
+    
+    mutating func nextInt() -> Int {
+        return Int(nextString())!
+    }
+    
+    mutating func nextInts(count n: Int) -> [Int] {
+        return (0 ..< n).map { _ in nextInt() }
+    }
+    
+    mutating func nextPairInts(count n: Int) -> ([Int], [Int]) {
+        var a = [Int]()
+        var b = [Int]()
+        for _ in 0 ..< n {
+            a.append(nextInt())
+            b.append(nextInt())
+        }
+        return (a, b)
+    }
+    
+    mutating func nextDouble() -> Double {
+        return Double(nextString())!
+    }
+}
